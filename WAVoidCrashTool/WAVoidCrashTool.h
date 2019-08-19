d0c25ccf3ad64bc6f6f21f586aa1aa0daa549c34
fanrenFRank <1778907544@qq.com>
2019-08-19 10:28:19 +0800

    sss

---
+++ WAVoidCrashTool/WAVoidCrashTool.h
@@ -0,0 +1,13 @@
+//
+//  WAVoidCrashTool.h
+//  WAVoidCrashTool
+//
+//  Created by LWW on 2018/3/2.
+//  Copyright © 2018年 LWW. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WAVoidCrashTool : NSObject
+
+@end
